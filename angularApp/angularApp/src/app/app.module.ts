e29f8a1c59d1b02c8babb9733ce5839f4fe7fc84
SaurabhDB <35756142+SaurabhDB@users.noreply.github.com>
2020-12-05 16:44:45 +0530

    ng - WIP : forms input

--- angularApp/angularApp/src/app/app.module.ts
+++ angularApp/angularApp/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,6 +7,7 @@ import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/angularmaterial/material/material.module';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,9 @@ import { MaterialModule } from './modules/angularmaterial/material/material.modu
     AppRoutingModule,
     MaterialModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularApp/angularApp/src/app/user-form/user-form.component.ts
+++ angularApp/angularApp/src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
-
-  constructor() { }
+  userDataForm: FormGroup;  
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.userDataForm = this.formBuilder.group({
+      userFirstName: [''], 
+      lastFirstName: [''],
+      userEmail: [''],
+      userPhoneNumber: [''],
+      userProfileImage: ['']
+    });
   }
 
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.userDataForm.get('userFirstName').value);
+    // this.userDataForm.setValue('userFirstName')
+    this.userDataForm.controls['userFirstName'].setValue(this.userDataForm.get('userFirstName').value);
+    console.log(this.userDataForm)
+
+}
 }
